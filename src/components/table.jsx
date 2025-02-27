7c2808a1946d230f493ece6bda9fcc9a85336145
Nigar <haciyeva.nigar2004@gmail.com>
2025-02-27 15:30:29 +0400

    .

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -2,8 +2,8 @@ import { Space, Table, Tag } from 'antd';
 const columns = [
   {
     title: 'Tracking Id',
-    dataIndex: 'TrackingId',
-    key: 'TrackingId',
+    dataIndex: 'trackingId',
+    key: 'trackingId',
     render: (text) => <a>{text}</a>,
   },
   {
@@ -22,21 +22,10 @@ const columns = [
     dataIndex: 'weight',
   },
 ];
-const data = [
-  {
-    key: '1',
-    TrackingId: '1234567',
-    Company: 'Trendyol',
-    index: '1',
-    weight:'2',
-  },
-  {
-    key: '2',
-    TrackingId: '567890',
-    Company: 'Trendyol',
-    index: '1',
-    weight:'2',
-  }
-];
-const CustomTable = () => <Table columns={columns} dataSource={data} />;
+
+const CustomTable = ({data}) =>{ 
+    return (
+        <Table columns={columns} dataSource={data} />
+      );
+}
 export default CustomTable;
