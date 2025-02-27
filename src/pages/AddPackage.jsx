049b3051435f7f031d9011057f14bd8cb5d090a6
Nigar <haciyeva.nigar2004@gmail.com>
2025-02-27 23:06:55 +0400

    addPackage system created

--- src/pages/AddPackage.jsx
+++ src/pages/AddPackage.jsx
@@ -1,58 +1,69 @@
 import { useState } from "react";
 import CustomTable from "../components/table";
 export default function AddPackage() {
-  const data = [
-    {
-      key: "1",
-      trackingId: "1234567",
-      Company: "Trendyol",
-      index: "1",
-      weight: "2",
-    },
-    {
-      key: "2",
-      trackingId: "567890",
-      Company: "Trendyol",
-      index: "1",
-      weight: "2",
-    },
-    {
-      key: "3",
-      trackingId: "111222",
-      Company: "Amazon",
-      index: "2",
-      weight: "3",
-    },
-  ];
-  const CustomTable = () => {
-    const [trackingId, setTrackingId] = useState("");
-    const [filteredData, setFilteredData] = useState(data);
-    const [count, setCount] = useState(0);
-    const handleCheck = () => {
-      const filtered = data.filter((item) =>
-        item.trackingId.includes(trackingId)
-      );
-      setFilteredData(filtered);
-      setCount(filtered.length);
-    };
-  };
+ const data = [
+ {
+ key: "1",
+ trackingId: "1234567",
+ Company: "Trendyol",
+ index: "1",
+ weight: "2",
+ },
+ {
+ key: "2",
+ trackingId: "567890",
+ Company: "Trendyol",
+ index: "1",
+ weight: "2",
+ },
+ {
+ key: "3",
+ trackingId: "111222",
+ Company: "Amazon",
+ index: "2",
+ weight: "3",
+ },
+ ];
+ const [trackingId, setTrackingId] = useState("");
+ const [filteredData, setFilteredData] = useState(data);
+ const [count, setCount] = useState(0);
+ const handleCheck = () => {
+  if( trackingId === ""){
+    alert( "Tracking Id cannot be empty!");
+ return}
+ else{
+ setFilteredData([...filteredData,{
+ key: filteredData.length+1,
+ trackingId: trackingId,
+ Company: "Amazon",
+ index: "2",
+ weight: "3",
+ }])
 
-  return (
-    <div className="addPackage">
-      <h2>Acceptance of packages</h2>
-      <div className="checking">
-        <input value={trackingId} />
-        <div className="buttons">
-          <span>12</span>
-          {<button className="check" onClick={handleCheck}>
-            Check
-          </button> }
-          <button className="status">Change status</button>
-        </div>
-      </div>
-      <div className="data">
-        { <CustomTable dataSource={filteredData} /> }
+ setTrackingId('');
+ }
+ };
+ const handleReset=()=>{
+  setFilteredData([]);
+ }
+
+ return (
+  <div className="addPackage">
+    <h2>Acceptance of packages</h2>
+    <div className="checking">
+      <input value={trackingId} placeholder="Tracking Id" onChange={e => setTrackingId(e.target.value)} maxLength={6} />
+      <div className="buttons">
+        <span>{filteredData.length}</span>
+        <button className="check" onClick={handleCheck}>
+          Check
+        </button>
+        <button className="status" onClick={handleReset}>Change status</button>
       </div>
     </div>
-  );
+    <div className="data">
+      <CustomTable data={filteredData.reverse()} />
+    </div>
+  </div>
+);
 }
+
